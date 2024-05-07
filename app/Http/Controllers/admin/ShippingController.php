c8c98776cc61c755884cb833900b336c7b22cae8
Himanshu2373 <8107himanshu@gmail.com>
2024-05-07 18:41:03 +0530

    updated

--- app/Http/Controllers/admin/ShippingController.php
+++ app/Http/Controllers/admin/ShippingController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Country;
 use Illuminate\Support\Facades\Validator;
-use App\Models\Shipping;
+use App\Models\ShippingCharge;
 
 
 class ShippingController extends Controller
@@ -14,21 +14,36 @@ class ShippingController extends Controller
     public function create()
     {
         $countries=Country::get();
+
+        $shippingCharges=ShippingCharge::select('shipping_charges.*','countries.name')->leftJoin('countries','countries.id','shipping_charges.country_id')->get();
         $data['countries']=$countries;
+        $data['shippingCharges']=$shippingCharges;
         return view('admin.shipping.create',$data);
     }
 
     public function store(Request $request){
-        $validator =Vaalidator::make($request->all(),[
+       
+        $validator =Validator::make($request->all(),[
             'country'=>'required',
             'amount'=>'required|numeric',
         ]);
 
         if($validator->passes()){
-            $shipping=new Shipping();
-            $shipping->country_id=$request->country;
-            $shipping->amount=$request->amount;
-            $shipping->save();
+
+            $count=ShippingCharge::where('country_id',$request->country)->count();
+        
+            if($count>0){
+                session()->flash('error','Shipping detailes already Exist');
+                return response()->json([
+                    'status'=>true
+                   
+                ]);
+            }
+
+            $shippingCharges=new ShippingCharge;
+            $shippingCharges->country_id=$request->country;
+            $shippingCharges->amount=$request->amount;
+            $shippingCharges->save();
             
             session()->flash('success','Shipping added Successfully');
             return response()->json([
@@ -44,5 +59,70 @@ class ShippingController extends Controller
             ]);
         }
 
+    }
+
+    public function edit($id, Request $request){
+        $shippingCharges=ShippingCharge::find($id);
+        if(empty($shippingCharges)){
+            $request->session()->flash('error', 'ShippingCharges Not Found');
+            return redirect()->route('shipping.create');
+        }
+        $countries=Country::get();
+        $data['countries']=$countries;
+        $data['shippingCharges']=$shippingCharges;
+
+        return view('admin.shipping.edit',$data);
+
+    }
+
+    public function update($id, Request $request){
+         
+        $shippingCharges=ShippingCharge::find($id);
+        if(empty($shippingCharges)){
+            $request->session()->flash('error', 'ShippingCharges Not Found');
+            return redirect()->route('shipping.create');
+        }
+
+        $validator =Validator::make($request->all(),[
+            'country'=>'required',
+            'amount'=>'required|numeric',
+        ]);
+
+        if($validator->passes()){
+            
+            $shippingCharges->country_id=$request->country;
+            $shippingCharges->amount=$request->amount;
+            $shippingCharges->save();
+            
+            session()->flash('success','Shipping updated Successfully');
+            return response()->json([
+                'success'=>'Shipping updated Successfully',
+                 'status'=>true
+            ]);
+        }
+        else{
+            return response()->json([
+                'status'=>false,
+                'errors'=>$validator->errors()
+
+            ]);
+        }
+    }
+    
+    public function destroy($id, Request $request){
+        $shippingCharges=ShippingCharge::find($id);
+
+        if(empty($shippingCharges)){
+            $request->session()->flash('error', 'ShippingCharges Not Found');
+            return redirect()->route('shipping.create');
+        }
+
+        $shippingCharges->delete();
+        $request->session()->flash('success', 'ShippingCharges Deleted Successfully');
+        return response()->json([
+            'status'=>true,
+            'message'=>'ShippingCharges deleted Successfully'
+        ]);
+               
     }
 }
--- resources/views/admin/shipping/create.blade.php
+++ resources/views/admin/shipping/create.blade.php
@@ -19,6 +19,7 @@
 <section class="content">
     <!-- Default box -->
     <div class="container-fluid">
+        @include('admin.message')
         <form action="" method="post" id="shippingForm" name="shippingForm">
         <div class="card">
             <div class="card-body">								
@@ -51,6 +52,48 @@
         </div>
     </form>
     </div>
+
+    <div class="card">
+        <div class="card-body">								
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table table-striped">
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Amount</th>
+                            <th>Action</th>
+                        </tr>
+                        @if($shippingCharges->isNotEmpty())
+                          @foreach ($shippingCharges as $shipping)
+                          <tr>
+                            <th>{{$shipping->id}}</th>
+                            <th>
+                                {{($shipping->country_id == 'Rest_of_world')? 'Rest of the world': $shipping->name}}
+                            </th>
+                            <th>${{$shipping->amount}}</th>
+                            <th>
+                                <a href="{{route('shipping.edit',$shipping->id)}}">
+                                    <svg class="filament-link-icon w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
+                                    </svg>
+                                </a>
+                                <a href="#" onclick="deleteShippingCharge({{$shipping->id}})" class="text-danger w-4 h-4 mr-1">
+                                    <svg wire:loading.remove.delay="" wire:target="" class="filament-link-icon w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path	ath fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+                                      </svg>
+                                </a>
+                            </th>
+                          </tr>
+                              
+                          @endforeach
+                          @endif
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- /.card -->
 </section>
 
@@ -64,7 +107,7 @@
           var element=$(this);
           $("button[type=submit]").prop('disabled',true);
          $.ajax({
-            url:'{{route("categories.store")}}',
+            url:'{{route("shipping.store")}}',
             type:'post',
             data: element.serializeArray(),
             dataType:'json',
@@ -73,36 +116,28 @@
                 
                 if(response["status"]==true){
 
-                    window.location.href="{{route('categories.index')}}";
-
-                    $("#name").removeClass('is-invalid')
-                    .siblings('p')
-                    .removeClass('invalid-feedback').html("");
-
-                    $("#slug").removeClass("is-invalid")
-                    .siblings('p')
-                    .removeClass('invalid-feedback').html("");
+                    window.location.href="{{route('shipping.create')}}";
 
                 }else{
                     var errors = response['errors'];
-                   if(errors['name']) {
-                    $("#name").addClass('is-invalid')
+                   if(errors['country']) {
+                    $("#country").addClass('is-invalid')
                     .siblings('p')
-                    .addClass('invalid-feedback').html(errors['name']);
+                    .addClass('invalid-feedback').html(errors['country']);
                     }
                    else{
-                    $("#name").removeClass('is-invalid')
+                    $("#country").removeClass('is-invalid')
                     .siblings('p')
                     .removeClass('invalid-feedback').html("");
                    }
 
-                   if(errors['slug']) {
-                    $("#slug").addClass('is-invalid')
+                   if(errors['amount']) {
+                    $("#amount").addClass('is-invalid')
                     .siblings('p')
-                    .addClass('invalid-feedback').html(errors['slug']);
+                    .addClass('invalid-feedback').html(errors['amount']);
                 }
                 else{
-                    $("#slug").removeClass("is-invalid")
+                    $("#amount").removeClass("is-invalid")
                     .siblings('p')
                     .removeClass('invalid-feedback').html("");
                 }
@@ -117,6 +152,31 @@
          
     });
 
+
+    function deleteShippingCharge(id){
+        var url='{{route('shipping.delete',"ID")}}'
+        var newUrl=url.replace('ID',id)
+
+       if(confirm("Are you sure you want to delete"))
+       {
+        $.ajax({
+            url: newUrl,
+            type:'delete',
+            data: {}, 
+            dataType:'json',
+            headers: {
+             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            success:function(response){
+                window.location.href="{{route('shipping.create')}}";
+               
+                }
+            });
+       }
+       else{
+        window.location.href="{{route('shipping.create')}}";
+       }
+     }
     
 </script>
 @endsection
